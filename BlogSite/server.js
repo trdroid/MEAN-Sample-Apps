35b062e44e3c96414d96166f44889d6238b2293c
gruprog <gruprog@gmail.com>
2016-02-12 23:43:21 -0500

    code files and a snapshot

--- BlogSite/server.js
+++ BlogSite/server.js
@@ -1,8 +1,15 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus'),
+	bodyParser = require('body-parser'),
+	morganLogger = require('morgan');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'DEV';
 
 var app = express();
+
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
 
 app.set('views', __dirname + '/server/views');
 
@@ -10,12 +17,28 @@ app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
 /*
+	setup the middleware for stylus by passing in configuration object
+*/
+app.use(stylus.middleware({
+	src: __dirname + '/public',
+	compile: compile
+}));
+
+app.use(morganLogger('dev'))
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//setup static routing to the public directory
+app.use(express.static(__dirname + '/public'));
+
+/*
 	a catch-all route handler
 
 	serve up an index page to the client and let angular take care of routing
 
-	In case of typos in the routes, angular could hang, so
-	an alternative approach is to make the server and the angular be aware of all the routes and be able to handle them
+	In case of typos in the routes, angular could hang.
+	An alternative approach is to make the server and the angular be aware of all the routes and be able to handle them
 */
 app.get('*', function(req, res) {
 	res.render('index');
