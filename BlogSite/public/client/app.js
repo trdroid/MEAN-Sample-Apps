7fd97739f3749d3d0518788afeff1ede0d353d1a
gruprog <gruprog@gmail.com>
2016-02-15 12:24:03 -0500

    angular app and server side partials and changes

---
+++ BlogSite/public/client/app.js
@@ -0,0 +1,46 @@
+/*
+	The main entry point to the Angular application
+
+	define module 'app' that depends on ngResource and ngRoute modules
+*/
+
+angular.module('app', ['ngResource', 'ngRoute']);
+
+/*
+	Define the client-side routes by calling the config function and requiring $routeProvider and $locationProvider
+*/
+angular.module('app').config(function($routeProvider, $locationProvider) {
+	/*
+		use $locationProvider to turn on HTML5 mode for routing
+
+		With recent versions of Angular, the head tag requires a base tag base(href="/") for routing to work properly.
+
+		so, server/layouts/main_layout.jade would look like:
+
+		doctype
+		html
+			head
+				base(href="/")  <-----------
+			body
+				block main-content
+				include scripts	
+	*/
+	$locationProvider.html5Mode(true);
+
+	/*
+		Use $routeProvider to define the routes
+	*/
+
+	$routeProvider
+		.when('/', { templateUrl: 'partials/root', controller: 'mainController'});
+		/* 
+			when the routing system determines that the value of location.path() is '/', it will inject the template 'partials/root' 
+			(or more precisely, it makes an XHR request to partials/root and injects its response)
+			into the ngView directive and makes 'mainController' its controller
+		*/
+});
+
+angular.module('app').controller('mainController', function($scope) {
+	$scope.title = "My First Blog";
+	$scope.content = "Welcome to my first blog";
+});
--- BlogSite/server.js
+++ BlogSite/server.js
@@ -18,6 +18,19 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 /*
+	The angular app sends XHR requests to /partials/:path, which are handled here.
+
+	For example, a request to /partials/root implies that req.params.path is root, which then 
+		attempts to render partials/root.jade. Since the views are configured to be found from /server/views
+		directory, the file /server/views/partials/root.jade would be rendered
+
+	The partials are therefore organized under /server/views/partials directory
+*/
+app.get('/partials/:path', function(req, res) {
+	res.render('partials/' + req.params.path);
+});
+
+/*
 	a catch-all route handler to serve up the index page when a request is made to a path that the server does not handle
 
 	The index page is served to the client where angular handles routing (as this is a single page application)
